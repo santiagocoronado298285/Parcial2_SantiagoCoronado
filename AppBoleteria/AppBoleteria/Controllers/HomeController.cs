74244736dd438e4f80771fc01ce930164b440eb0
Santiago C <scoronado@integrupo.com>
2023-04-01 18:55:25 -0500

    created creating the template in ASP.NET

---
+++ AppBoleteria/AppBoleteria/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using AppBoleteria.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AppBoleteria.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AppBoleteria/AppBoleteria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppBoleteria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
