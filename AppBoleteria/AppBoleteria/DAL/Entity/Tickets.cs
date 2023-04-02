7c74a2570dd9e5ac133c4c7a6e9d56ccdf85252d
Santiago C <scoronado@integrupo.com>
2023-04-01 19:15:36 -0500

    created class Tickets

---
+++ AppBoleteria/AppBoleteria/DAL/Entity/Tickets.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppBoleteria.DAL.Entity
+{
+    public class Tickets
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+
+        [Display(Name = "Fecha de uso Boleta")]
+        public DateTime UseDate { get; set; }
+
+        [Display(Name = "Fecha de modificación")]
+        public DateTime? ModifiedDate { get; set; }
+
+        public bool IsUse { get; set; }
+
+        public string EntranceGate { get; set; }
+
+    }
+}
